29162fcbb45c1f151b68c024dcec7c4396605834
rzwibowo <d7revenge@gmail.com>
2018-05-29 14:55:50 +0700

    inisialisasi apps

---
+++ application/views/layout/v_foot.php
@@ -0,0 +1,10 @@
+	<!--   Core JS Files   -->
+    <script src="/assets/js/core/jquery.min.js"></script>
+    <script src="/assets/js/core/popper.min.js"></script>
+    <script src="/assets/js/bootstrap-material-design.js"></script>
+
+    <!-- Material Dashboard Core initialisations of plugins and Bootstrap Material Design Library -->
+    <script src="/assets/js/material-dashboard.js?v=2.0.0"></script>
+
+  </body>
+</html>
