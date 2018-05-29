c16438fc15aaf489c9187e6d0d844ffd850c8e28
rzwibowo <d7revenge@gmail.com>
2018-05-29 18:35:31 +0700

    db

---
+++ spk_bsw.sql
@@ -0,0 +1,125 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.5.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 29, 2018 at 01:34 PM
+-- Server version: 10.1.21-MariaDB
+-- PHP Version: 7.0.16
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `spk_bsw`
+--
+CREATE DATABASE IF NOT EXISTS `spk_bsw` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `spk_bsw`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `beasiswa`
+--
+
+CREATE TABLE `beasiswa` (
+  `kode_beasiswa` varchar(18) NOT NULL,
+  `nim` int(11) NOT NULL,
+  `jumlah_beasiswa` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kriteria`
+--
+
+CREATE TABLE `kriteria` (
+  `kode_kriteria` int(3) NOT NULL,
+  `nama_kriteria` varchar(20) NOT NULL,
+  `bobot_kriteria` double NOT NULL,
+  `nim` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `mahasiswa`
+--
+
+CREATE TABLE `mahasiswa` (
+  `nim` int(11) NOT NULL,
+  `nama` varchar(30) NOT NULL,
+  `thn_angkatan` int(4) NOT NULL,
+  `jenis_kelamin` enum('laki-laki','perempuan','','') NOT NULL,
+  `tempat_lahir` varchar(20) NOT NULL,
+  `tgl_lahir` date NOT NULL,
+  `alamat` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pengelola`
+--
+
+CREATE TABLE `pengelola` (
+  `nip` int(11) NOT NULL,
+  `username` varchar(15) NOT NULL,
+  `password` varchar(32) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `beasiswa`
+--
+ALTER TABLE `beasiswa`
+  ADD KEY `nim` (`nim`);
+
+--
+-- Indexes for table `kriteria`
+--
+ALTER TABLE `kriteria`
+  ADD PRIMARY KEY (`kode_kriteria`),
+  ADD KEY `nim` (`nim`);
+
+--
+-- Indexes for table `mahasiswa`
+--
+ALTER TABLE `mahasiswa`
+  ADD PRIMARY KEY (`nim`);
+
+--
+-- Indexes for table `pengelola`
+--
+ALTER TABLE `pengelola`
+  ADD PRIMARY KEY (`nip`);
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `beasiswa`
+--
+ALTER TABLE `beasiswa`
+  ADD CONSTRAINT `beasiswa_ibfk_1` FOREIGN KEY (`nim`) REFERENCES `mahasiswa` (`nim`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `kriteria`
+--
+ALTER TABLE `kriteria`
+  ADD CONSTRAINT `kriteria_ibfk_1` FOREIGN KEY (`nim`) REFERENCES `mahasiswa` (`nim`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
