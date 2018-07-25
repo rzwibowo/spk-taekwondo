cd19596f4e8e82508a03d0f07e391d56bcebcbf7
rzwibowo <d7revenge@gmail.com>
2018-07-25 20:41:40 +0700

    db update

--- spk_bsw.sql
+++ spk_bsw.sql
@@ -1,15 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.0
+-- version 4.6.5.2
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: 23 Jun 2018 pada 15.52
--- Versi Server: 10.1.24-MariaDB
--- PHP Version: 7.1.6
+-- Generation Time: Jul 25, 2018 at 03:38 PM
+-- Server version: 10.1.21-MariaDB
+-- PHP Version: 7.0.16
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -21,48 +19,49 @@ SET time_zone = "+00:00";
 --
 -- Database: `spk_bsw`
 --
+CREATE DATABASE IF NOT EXISTS `spk_bsw` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `spk_bsw`;
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `beasiswa`
+-- Table structure for table `beasiswa`
 --
 
 CREATE TABLE `beasiswa` (
   `kode_beasiswa` varchar(18) NOT NULL,
-  `id_mahasiswa` int(11) NOT NULL,
+  `nim` varchar(10) NOT NULL,
   `jumlah_beasiswa` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `kriteria`
+-- Table structure for table `kriteria`
 --
 
 CREATE TABLE `kriteria` (
-  `id_kriteria` int(11) NOT NULL,
-  `kode_kriteria` varchar(255) NOT NULL,
+  `kode_kriteria` int(3) NOT NULL,
   `nama_kriteria` varchar(20) NOT NULL,
-  `bobot_kriteria` double NOT NULL
+  `bobot_kriteria` double(9,1) NOT NULL,
+  `nim` varchar(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data untuk tabel `kriteria`
+-- Dumping data for table `kriteria`
 --
 
-INSERT INTO `kriteria` (`id_kriteria`, `kode_kriteria`, `nama_kriteria`, `bobot_kriteria`) VALUES
-(2, 'B003', 'Test', 23);
+INSERT INTO `kriteria` (`kode_kriteria`, `nama_kriteria`, `bobot_kriteria`, `nim`) VALUES
+(1, 'Test', 23.0, '223244');
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `mahasiswa`
+-- Table structure for table `mahasiswa`
 --
 
 CREATE TABLE `mahasiswa` (
-  `id_mahasiswa` int(11) NOT NULL,
-  `nim` int(11) NOT NULL,
+  `nim` varchar(10) NOT NULL,
   `nama` varchar(30) NOT NULL,
   `thn_angkatan` int(4) NOT NULL,
   `jenis_kelamin` enum('laki-laki','perempuan','','') NOT NULL,
@@ -77,17 +76,18 @@ CREATE TABLE `mahasiswa` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data untuk tabel `mahasiswa`
+-- Dumping data for table `mahasiswa`
 --
 
-INSERT INTO `mahasiswa` (`id_mahasiswa`, `nim`, `nama`, `thn_angkatan`, `jenis_kelamin`, `tempat_lahir`, `tgl_lahir`, `alamat`, `ipk`, `kendaraan`, `pgh_orangtua`, `pkj_orangtua`, `jml_tanggungan`) VALUES
-(3, 223244, 'Sahrun', 300, 'laki-laki', 'Jakarta', '2018-06-12', 'Jalan R.A.Kartini No. 9, RT. 10 / RW. 4, Cilandak Barat, Cilandak, RT.10/RW.4, Cilandak Bar., Cilandak, Kota Jakarta Selatan, Daerah Khusus Ibukota Jakarta 12440', 0, '', 0, '', 0),
-(4, 234423, 'Ardi', 2003, 'laki-laki', 'Jakarta', '2018-06-23', 'Jalan R.A.Kartini No. 9, RT. 10 / RW. 4, Cilandak Barat, Cilandak, RT.10/RW.4, Cilandak Bar., Cilandak, Kota Jakarta Selatan, Daerah Khusus Ibukota Jakarta 12440', 12, 'sdsads', 2321, 'sadsd', 23);
+INSERT INTO `mahasiswa` (`nim`, `nama`, `thn_angkatan`, `jenis_kelamin`, `tempat_lahir`, `tgl_lahir`, `alamat`, `ipk`, `kendaraan`, `pgh_orangtua`, `pkj_orangtua`, `jml_tanggungan`) VALUES
+('223244', 'Sahrun', 300, 'laki-laki', 'Jakarta', '2018-06-12', 'Jalan R.A.Kartini No. 9, RT. 10 / RW. 4, Cilandak Barat, Cilandak, RT.10/RW.4, Cilandak Bar., Cilandak, Kota Jakarta Selatan, Daerah Khusus Ibukota Jakarta 12440', 0, '', 0, '', 0),
+('234423', 'Ardi', 2003, 'laki-laki', 'Jakarta', '2018-06-23', 'Jalan R.A.Kartini No. 9, RT. 10 / RW. 4, Cilandak Barat, Cilandak, RT.10/RW.4, Cilandak Bar., Cilandak, Kota Jakarta Selatan, Daerah Khusus Ibukota Jakarta 12440', 12, 'sdsads', 2321, 'sadsd', 23),
+('29111', 'Handa Yono', 2019, 'laki-laki', 'Asgard', '2002-01-07', 'Wakanda Foreva', 7, 'Polygon', 20000, 'Buruh Tani', 7);
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `pengelola`
+-- Table structure for table `pengelola`
 --
 
 CREATE TABLE `pengelola` (
@@ -97,7 +97,7 @@ CREATE TABLE `pengelola` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data untuk tabel `pengelola`
+-- Dumping data for table `pengelola`
 --
 
 INSERT INTO `pengelola` (`nip`, `username`, `password`) VALUES
@@ -111,19 +111,21 @@ INSERT INTO `pengelola` (`nip`, `username`, `password`) VALUES
 -- Indexes for table `beasiswa`
 --
 ALTER TABLE `beasiswa`
-  ADD KEY `nim` (`id_mahasiswa`);
+  ADD PRIMARY KEY (`kode_beasiswa`),
+  ADD KEY `nim` (`nim`);
 
 --
 -- Indexes for table `kriteria`
 --
 ALTER TABLE `kriteria`
-  ADD PRIMARY KEY (`id_kriteria`);
+  ADD PRIMARY KEY (`kode_kriteria`),
+  ADD KEY `nim` (`nim`);
 
 --
 -- Indexes for table `mahasiswa`
 --
 ALTER TABLE `mahasiswa`
-  ADD PRIMARY KEY (`id_mahasiswa`);
+  ADD PRIMARY KEY (`nim`);
 
 --
 -- Indexes for table `pengelola`
@@ -139,27 +141,27 @@ ALTER TABLE `pengelola`
 -- AUTO_INCREMENT for table `kriteria`
 --
 ALTER TABLE `kriteria`
-  MODIFY `id_kriteria` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
---
--- AUTO_INCREMENT for table `mahasiswa`
---
-ALTER TABLE `mahasiswa`
-  MODIFY `id_mahasiswa` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `kode_kriteria` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 --
 -- AUTO_INCREMENT for table `pengelola`
 --
 ALTER TABLE `pengelola`
   MODIFY `nip` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 --
--- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
+-- Constraints for dumped tables
 --
 
 --
--- Ketidakleluasaan untuk tabel `beasiswa`
+-- Constraints for table `beasiswa`
 --
 ALTER TABLE `beasiswa`
-  ADD CONSTRAINT `beasiswa_ibfk_1` FOREIGN KEY (`id_mahasiswa`) REFERENCES `mahasiswa` (`id_mahasiswa`) ON DELETE CASCADE ON UPDATE CASCADE;
-COMMIT;
+  ADD CONSTRAINT `beasiswa_ibfk_1` FOREIGN KEY (`nim`) REFERENCES `mahasiswa` (`nim`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `kriteria`
+--
+ALTER TABLE `kriteria`
+  ADD CONSTRAINT `kriteria_ibfk_1` FOREIGN KEY (`nim`) REFERENCES `mahasiswa` (`nim`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
