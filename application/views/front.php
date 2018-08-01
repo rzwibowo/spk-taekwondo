5d1754dfeaa1bbd1340c7885c0d57aea7165dfa1
rzwibowo <rizkiadigunow@gmail.com>
2018-08-01 15:54:37 +0700

    fix api call, tambah statistik kriteria

--- application/views/front.php
+++ application/views/front.php
@@ -15,6 +15,22 @@
             </div>
         </div>
     </div>
+    <div class="col-md-6">
+        <div class="card text-white bg-warning">
+            <div class="card-body pb-0" style="z-index: 1">
+                <div class="btn-group float-right">
+                    <a href="<?php echo site_url() ?>/kriteria" class="btn btn-transparent">
+                        <i class="icon-pencil"></i>
+                    </a>
+                </div>
+                <div class="text-value">{{ count_kriteria }}</div>
+                <div>Kriteria Terdata</div>
+            </div>
+            <div class="chart-wrapper mt-3 mx-3" style="height:70px;">
+                <i class="fa fa-tasks ikon-dash"></i>
+            </div>
+        </div>
+    </div>
 </div>
 <script type="text/javascript">
 var app = new Vue({
@@ -23,15 +39,26 @@ var app = new Vue({
     this.GetData();
   },
   data: {
-  	count_mhs: 0
+  	count_mhs: 0,
+    count_kriteria: 0
   },
   methods: {
     GetData()
     {
         axios
-            .post('http://localhost/spk-beasiswa/index.php/api/mahasiswa/mahasiswas')
+            .post(locationServer+'/api/mahasiswa/mahasiswas', {body: {}})
             .then(response => {
-                this.count_mhs = response.data.length;
+                this.count_mhs = response.data.length
+            })
+            .catch(error => {
+                console.log(error)
+                this.errored = true
+            })
+            .finally(() => this.loading = false )
+        axios
+            .post(locationServer+'/api/kriteria/kriterias', {body: {}})
+            .then(response => {
+                this.count_kriteria = response.data.length
             })
             .catch(error => {
                 console.log(error)
