41e6b28b17e553aab2dd4d3216aa29e23f48efe9
Rizki Adiguno Wibowo <rizkiadigunow@gmail.com>
2019-04-09 21:48:02 +0700

    api biaya latihan

---
+++ application/controllers/api/BiayaLatihan.php
@@ -0,0 +1,81 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+// This can be removed if you use __autoload() in config.php OR use Modular Extensions
+/** @noinspection PhpIncludeInspection */
+require APPPATH . '/libraries/REST_Controller.php';
+
+// use namespace
+use Restserver\Libraries\REST_Controller;
+
+/**
+ * This is an example of a few basic user interaction methods you could use
+ * all done with a hardcoded array
+ *
+ * @package         CodeIgniter
+ * @subpackage      Rest Server
+ * @category        Controller
+ * @author          Phil Sturgeon, Chris Kacerguis
+ * @license         MIT
+ * @link            https://github.com/chriskacerguis/codeigniter-restserver
+ */
+class BiayaLatihan extends REST_Controller {
+
+    function __construct($config = 'rest')
+    {
+        // Construct the parent class
+        parent::__construct($config);
+        $this->load->model('ModelBiayaLatihan');
+
+    }
+
+    public function ambilBya_get()
+    {
+        $data = $this->ModelBiayaLatihan->GetBiaya()->result();
+        if ($data) {
+            $this->set_response($data, REST_Controller::HTTP_OK);
+        } else {
+            $this->set_response(array('error' => 'Tidak ditemukan data'),  REST_Controller::HTTP_NOT_FOUND);
+        }
+    }
+    public function simpanBya_post()
+    {
+        $TL = (object) $this->post('body');
+        if ($this->ModelBiayaLatihan->InsertBiaya($TL)) {
+            $this->set_response(array('status' => 'sukses'), REST_Controller::HTTP_CREATED);
+        } else {
+            $this->set_response(array('error' => 'Error saat simpan data'),  REST_Controller::HTTP_BAD_REQUEST);
+        }
+    }
+    public function updateBya_put()
+    {
+        $TL = (object) $this->put('body');
+        if ($this->ModelBiayaLatihan->UpdateBiaya($TL)){
+            $this->set_response(array('status' => 'sukses'), REST_Controller::HTTP_CREATED);
+        } else {
+            $this->set_response(array('error' => 'Error saat simpan data'),  REST_Controller::HTTP_BAD_REQUEST);
+        }
+    }
+    public function ambilByaDenganId_get($Id)
+    {
+        # code...
+        $where = array('detail_kriteria.id_detail_kriteria'=>$Id);
+        $TL = $this->ModelBiayaLatihan->GetBiayaById($where)->result();
+        if ($TL) {
+            $this->set_response($TL[0], REST_Controller::HTTP_CREATED);
+        } else {
+            $this->set_response(array('error' => 'Tidak ditemukan data'),  REST_Controller::HTTP_NOT_FOUND);
+        }
+    }
+    public function hapusBya_delete($Id)
+    {
+        $this->ModelBiayaLatihan->Delete($Id);
+        if ($Id <= 0)
+        {
+            $this->response(NULL, REST_Controller::HTTP_BAD_REQUEST); // BAD_REQUEST (400) being the HTTP response code
+        }
+        $this->set_response(array('status' => 'sukses'), REST_Controller::HTTP_NO_CONTENT); // NO_CONTENT (204) being the HTTP response code
+    }
+
+}
