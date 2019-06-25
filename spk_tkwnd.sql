4d82e9e546ce637229ab7fd3453d74b675fd966f
Rizki Adiguno Wibowo <rizkiadigunow@gmail.com>
2019-06-25 21:23:37 +0700

    update db

--- spk_tkwnd.sql
+++ spk_tkwnd.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 22, 2019 at 04:05 PM
+-- Generation Time: Jun 25, 2019 at 04:22 PM
 -- Server version: 10.1.34-MariaDB
 -- PHP Version: 7.2.7
 
